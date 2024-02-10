5ec4487ade4b99553ac7f50fb8fdf0a839b560da
annmaryvinod <annmarypriyavinod@gmail.com>
2024-02-10 15:15:15 +0530

    design edits

--- client/src/App.js
+++ client/src/App.js
@@ -1,94 +1,18 @@
 import "./App.css";
+import Header from "./components/Header";
+import  Post from "./components/Post";
+import {Routes} from "react-router-dom";
 
 function App() {
   return (
     <main>
-      <header>
-        <a href="" className="logo">
-          MyBlog
-        </a>
+      <Header/>
+      <Post/>
+      <Post/>
+      <Post/>
 
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
-
-      <div className="post">
-        <div className="images">
-          <img
-            src="https://assets-global.website-files.com/62fcc3b5e3bdda3ad0d34c11/6536ee11999e4b293259d0fa_medium-p-1600.jpg"
-            alt=""
-          />
-        </div>
-        <div className="texts">
-          <h2>The Pros & Cons Of Blogging On Medium</h2>
-          <p className="info">
-            <a href="" className="author">
-              Atticus Finch
-            </a>
-            <time>2024-02-10 11:50</time>
-          </p>
-          <p className="summary">
-            Share on LinkedInShare on LinkedIn Share on XShare on X Share on
-            FacebookShare on Facebook Share on EmailShare on Email If you're
-            entering the world of blogging, chances are you’ve heard of
-            Medium—the online blogging platform with more than 170 million
-            readers.
-          </p>
-        </div>
-      </div>
-
-      <div className="post">
-        <div className="images">
-          <img
-            src="https://assets-global.website-files.com/62fcc3b5e3bdda3ad0d34c11/6536ee11999e4b293259d0fa_medium-p-1600.jpg"
-            alt=""
-          />
-        </div>
-
-        <div className="texts">
-          <h2>The Pros & Cons Of Blogging On Medium</h2>
-          <p className="info">
-            <a href="" className="author">
-              Atticus Finch
-            </a>
-            <time>2024-02-10 11:50</time>
-          </p>
-          <p className="summary">
-            Share on LinkedInShare on LinkedIn Share on XShare on X Share on
-            FacebookShare on Facebook Share on EmailShare on Email If you're
-            entering the world of blogging, chances are you’ve heard of
-            Medium—the online blogging platform with more than 170 million
-            readers.
-          </p>
-        </div>
-      </div>
-
-      <div className="post">
-        <div className="images">
-          <img
-            src="https://assets-global.website-files.com/62fcc3b5e3bdda3ad0d34c11/6536ee11999e4b293259d0fa_medium-p-1600.jpg"
-            alt=""
-          />
-        </div>
-        <div className="texts">
-          <h2>The Pros & Cons Of Blogging On Medium</h2>
-          <p className="info">
-            <a href="" className="author">
-              Atticus Finch
-            </a>
-            <time>2024-02-10 11:50</time>
-          </p>
-          <p className="summary">
-            Share on LinkedInShare on LinkedIn Share on XShare on X Share on
-            FacebookShare on Facebook Share on EmailShare on Email If you're
-            entering the world of blogging, chances are you’ve heard of
-            Medium—the online blogging platform with more than 170 million
-            readers.
-          </p>
-        </div>
-      </div>
+      
+      
     </main>
   );
 }
---
+++ client/src/components/Post.js
@@ -0,0 +1,32 @@
+const Post = () => {
+    return ( 
+        <div className="post">
+        <div className="images">
+          <img
+            src="https://assets-global.website-files.com/62fcc3b5e3bdda3ad0d34c11/6536ee11999e4b293259d0fa_medium-p-1600.jpg"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>The Pros & Cons Of Blogging On Medium</h2>
+          <p className="info">
+            <a href="" className="author">
+              Atticus Finch
+            </a>
+            <time>2024-02-10 11:50</time>
+          </p>
+          <p className="summary">
+            Share on LinkedInShare on LinkedIn Share on XShare on X Share on
+            FacebookShare on Facebook Share on EmailShare on Email If you're
+            entering the world of blogging, chances are you’ve heard of
+            Medium—the online blogging platform with more than 170 million
+            readers.
+          </p>
+        </div>
+      </div>
+
+
+     );
+}
+ 
+export default Post;
